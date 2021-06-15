db03e6ed22d676d0577629d1601a2b1f9354bda6
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-06-15 17:52:25 -0300

    Aula 2

---
+++ estrutura-dados-algoritmos-java/src/com/guilherme/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guilherme.estruturadados.vetor.teste;
+
+import com.guilherme.estruturadados.vetor.Vetor;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Gui e Di
+ */
+public class Aula03 {
+
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+
+    }
+
+}
