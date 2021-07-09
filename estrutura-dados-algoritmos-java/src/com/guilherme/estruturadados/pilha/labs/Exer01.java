91ce158a9b9a2355ae8289b9b89bfb32eda1f311
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-07-09 19:13:03 -0300

    Exercicios 01 02 Pilhas

--- estrutura-dados-algoritmos-java/src/com/guilherme/estruturadados/pilha/labs/Exer01.java
+++ estrutura-dados-algoritmos-java/src/com/guilherme/estruturadados/pilha/labs/Exer01.java
@@ -5,10 +5,53 @@
  */
 package com.guilherme.estruturadados.pilha.labs;
 
+import com.guilherme.estruturadados.pilha.Pilha;
+import java.util.Scanner;
+
 /**
  *
  * @author Gui e Di
  */
 public class Exer01 {
-    
+
+    public static void main(String[] args) {
+
+        Pilha<Integer> pilha = new Pilha<>();
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println("Entre com um número:");
+
+            int num = scan.nextInt();
+
+            if (num % 2 == 0) {
+                System.out.println("Empilhando o número  " + num);
+                pilha.empilha(num);
+            } else {
+
+                Integer desempilhado = pilha.desempilha();
+
+                if (desempilhado == null) {
+                    System.out.println("Pilha está vazia");
+                } else {
+                    System.out.println("Número impar, desempilhando um elemento da pilha"
+                            + desempilhado);
+                }
+            }
+        }
+
+        System.out.println("Todos os números foram lidos, desempilhando números da pilha");
+
+        while (!pilha.estaVazia()) {
+            
+                System.out.println("Desempilhando um elemento da pilha"
+                        + pilha.desempilha());
+            
+        }
+        
+        System.out.println("Todos os elementos foram desempilhados");
+
+    }
 }
---
+++ estrutura-dados-algoritmos-java/src/com/guilherme/estruturadados/pilha/labs/Exer02.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guilherme.estruturadados.pilha.labs;
+
+import com.guilherme.estruturadados.pilha.Pilha;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gui e Di
+ */
+public class Exer02 {
+
+    public static void main(String[] args) {
+        Pilha<Integer> par = new Pilha<>();
+        Pilha<Integer> impar = new Pilha<>();
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Entre com um numero: ");
+
+            int num = scan.nextInt();
+
+            if (num == 0) {
+
+                Integer desempilhado = par.desempilha();
+
+                if (desempilhado == null) {
+                    System.out.println("Pilha par vazia.");
+                } else {
+                    System.out.println("Desempilhado da pilha par: " + desempilhado);
+                }
+
+                desempilhado = impar.desempilha();
+
+                if (desempilhado == null) {
+                    System.out.println("Pilha par vazia.");
+                } else {
+                    System.out.println("Desempilhado da pilha impar: " + desempilhado);
+                }
+
+            } else if (num % 2 == 0) {
+                System.out.println("Numero par, empilhando na pilha par: " + num);
+                par.empilha(num);
+
+            } else {
+                System.out.println("Numero impar, empilhando na pilha impar: " + num);
+                impar.empilha(num);
+            }
+
+        }
+        
+        System.out.println("Desempilhando todos os números da pilha par");
+        
+        while(!par.estaVazia()){
+            System.out.println("Desempilhando da pilha par: "+par.desempilha());
+        }
+        
+        System.out.println("Desempilhando todos os números da pilha impar");
+        
+        while(!impar.estaVazia()){
+            System.out.println("Desempilhando da pilha impar: "+impar.desempilha());
+        }
+        
+        
+        
+
+    }
+}
