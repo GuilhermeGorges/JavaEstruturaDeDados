9a6078c55ee629af57a683596b1a761e8a5b1c60
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-07-10 07:16:49 -0300

    Exercicio 04 Pilhas

---
+++ estrutura-dados-algoritmos-java/src/com/guilherme/estruturadados/pilha/labs/Exer04.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guilherme.estruturadados.pilha.labs;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Gui e Di
+ */
+public class Exer04 {
+
+    public static void main(String[] args) {
+        
+        Stack<Livro> pilha = new Stack<>();
+
+        Livro livro1 = new Livro();
+        livro1.setNome("O senhor dos aneis 1 ");
+        livro1.setAutor("Tolkien");
+        livro1.setAnoLancamento(1960);
+        livro1.setIsbn("123456");
+
+        Livro livro2 = new Livro();
+        livro2.setNome("O senhor dos aneis 2");
+        livro2.setAutor("Tolkien");
+        livro2.setAnoLancamento(1961);
+        livro2.setIsbn("234567");
+
+        Livro livro3 = new Livro();
+        livro3.setNome("O senhor dos aneis 3 ");
+        livro3.setAutor("Tolkien");
+        livro3.setAnoLancamento(1962);
+        livro3.setIsbn("345678");
+
+        Livro livro4 = new Livro();
+        livro4.setNome("O hobbit");
+        livro4.setAutor("Tolkien");
+        livro4.setAnoLancamento(1959);
+        livro4.setIsbn("456789");
+
+        System.out.println("Pìlha de livros criada, pilha de livros vazia? " + pilha.isEmpty());
+
+        System.out.println("Empilhando livros na pilha:");
+
+        pilha.push(livro1);
+        pilha.push(livro2);
+        pilha.push(livro3);
+        pilha.push(livro4);
+
+        System.out.println(pilha.size()+ " livros foram empilhados:");
+        System.out.println(pilha);
+        
+        System.out.println("Pìlha de livros criada, pilha de livros vazia? " + pilha.isEmpty());
+
+        System.out.println("Espiando o topo da pilha: "+ pilha.peek());
+        
+        System.out.println("Desempilhando livros da pilha:");
+        
+        while(!pilha.isEmpty()){
+            System.out.println("Desempilhando livro "+ pilha.pop());
+        }
+        
+        System.out.println("Todos os livros foram desempilhados, pilha vazia: " + pilha.isEmpty());
+    }
+}
---
+++ estrutura-dados-algoritmos-java/src/com/guilherme/estruturadados/pilha/labs/Exer05.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guilherme.estruturadados.pilha.labs;
+
+import com.guilherme.estruturadados.pilha.Pilha;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gui e Di
+ */
+public class Exer05 {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite um palindromo: ");
+        String palavra = scan.nextLine();
+        
+        imprimeResultado(palavra);
+         
+    }
+    
+    public static void imprimeResultado(String palavra){
+        if(testaPalindromo(palavra)){
+            System.out.println(palavra + " é um palindromo");
+        } else {            
+            System.out.println(palavra + " não é um palindromo");
+        }  
+    }
+    
+    public static boolean testaPalindromo(String palavra){
+        
+        Pilha<Character> pilha = new Pilha<>();
+        
+        for (int i = 0; i < palavra.length(); i++){
+            pilha.empilha(palavra.charAt(i));
+        }
+        
+        String palavraInversa = "";
+        
+        while(!pilha.estaVazia()){
+            palavraInversa += pilha.desempilha();
+            
+        }
+        
+        if (palavraInversa.equalsIgnoreCase(palavra)){
+            return true;
+        }   
+        
+        return false;
+    }
+}
