1bc7081a7e792af42bb008fdfdc95649ca52d7c2
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-07-10 07:59:13 -0300

    Exercicio 05 Pilhas

---
+++ estrutura-dados-algoritmos-java/src/com/guilherme/estruturadados/pilha/labs/Exer06.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guilherme.estruturadados.pilha.labs;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ *
+ * @author Gui e Di
+ */
+public class Exer06 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite um palindromo: ");
+        String expressao = scan.nextLine();
+        
+        imprimeResultado(expressao);
+
+    }
+    
+    public static void imprimeResultado(String expressao){
+        
+        System.out.println(expressao + " está balanceada? " + verificaSimbolosBalanceador(expressao));
+    }
+    
+    final static String ABRE = "({[";
+    final static String FECHA = ")}]";
+    
+    
+
+    public static boolean verificaSimbolosBalanceador(String expressao) {
+
+        Stack<Character> pilha = new Stack<>();
+        int index = 0;
+        char simbolo, topo;
+        
+        
+        while (index < expressao.length()){
+            simbolo = expressao.charAt(index);
+            
+            if (ABRE.indexOf(simbolo) > -1){
+                pilha.push(simbolo);
+            } else if (FECHA.indexOf(simbolo) > -1){
+                
+                if (pilha.isEmpty()){
+                    return false;
+                } else {
+                    topo = pilha.pop();
+                    
+                    if(ABRE.indexOf(topo) != FECHA.indexOf(simbolo)){
+                        return false;
+                    }
+                }
+            }     
+            index++;
+        }
+        return true;
+    }
+}
